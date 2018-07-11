ae8b8d2751533db41f255d9339262ba32eea4fa4
Katrin Leinweber <9948149+katrinleinweber@users.noreply.github.com>
2018-07-11 14:53:17 +0200

    Initial commit

---
+++ fibo.py
@@ -0,0 +1,17 @@
+def fib(n):    # write Fibonacci series up to n
+    a, b = 0, 1
+    while a < n:
+        print(a)
+        a, b = b, a+b
+    print()
+
+def fib2(n):   # return Fibonacci series up to n
+    """
+    As described by Fibo et al.
+    """
+    result = []
+    a, b = 0, 1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
